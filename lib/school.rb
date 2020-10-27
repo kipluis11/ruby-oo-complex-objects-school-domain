3b1aa98277bf3bf8f110b97a816784eef7a2eb18
kip_parcell <kipluis@gmail.com>
2020-10-27 10:49:38 -0700

    commit tree bbb6b728b57da25b4f0018b562c69aa116a14d4b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+
+class School
+    attr_accessor :school, :roster, :length
+
+    def initialize(school)
+        @school = school
+        @roster = {} 
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] << "#{student}"
+    end
+
+    def grade(grade)
+        roster.detect do |yes, st|
+            if yes == grade
+                return st
+            end
+        end
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, st|
+            sorted[grade] = st.sort
+        end
+        sorted
+    end
+
+end
